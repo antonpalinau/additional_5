0e49fae092a26736180c0916209d437cb57b3b2e
Anton Palinau <antonpalinau@gmail.com>
2017-09-29 02:30:57 +0300

    solvedBrackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  for (var i = str.length - 1; i >= 0; i--) {
+    console.log(i);
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][0]) {
+        console.log(str);
+        str = str.replace(bracketsConfig[j].join(''), '');
+        console.log(str);
+      }
+    }
+  }
+  if (str === '') {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
